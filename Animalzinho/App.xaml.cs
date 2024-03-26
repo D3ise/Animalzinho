2aec27fa710c666d5b8e0c02d244e91476df291b
Deise <deiseelen17@gmail.com>
2024-03-26 10:35:27 -0300

    Novo projeto

---
+++ Animalzinho/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Animalzinho;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Animalzinho/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Animalzinho;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
