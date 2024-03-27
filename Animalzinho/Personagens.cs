ba45991e6b4ed9d9dc9b6285aac7d9eafd37de14
deise <deiseelen17@gmail.com>
2024-03-27 11:47:04 -0300

    salvar

---
+++ Animalzinho/Personagens.cs
@@ -0,0 +1,19 @@
+namespace Animalzinho;
+
+public class Personagens
+{
+
+protected double fome;
+protected double sede;
+protected double tristeza;
+
+public Personagens()
+{
+    fome = 0;
+    sede = 0;
+    trizteza = 0;
+}
+
+
+
+}
